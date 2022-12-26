c713a822b2dfc13a6fcc55e6d7915d8ebfe56a40
Elif <elifnurbirer1994@gmail.com>
2022-12-25 18:58:44 -0800

    This is Question2 commit

---
+++ src/main/java/Day02/Question2.java
@@ -0,0 +1,10 @@
+package Day02;
+
+public class Question2 {
+    public static void main(String[] args) {
+        int i=10;
+        int j=20;
+        int k= (j+=i)/5;
+        System.out.println(i + ":" + j + ":" + k); //Output ===> 10:30:6
+    }
+}
